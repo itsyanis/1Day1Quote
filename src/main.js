1ad11300f7a4a0496803fb9e739623e564a012b3
itsyanis <yanis.maafi@viacesi.fr>
2025-03-13 23:26:46 +0100

    feat: add Vercel Analytics integration for performance tracking

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import "./assets/main.css";
 import { createApp } from "vue";
 import { createHead } from "@vueuse/head";
+import { inject } from "@vercel/analytics";
 import App from "./App.vue";
 
 const app = createApp(App);
 const head = createHead();
+inject({
+  mode: "production",
+});
 
 app.use(head);
 app.mount("#app");
